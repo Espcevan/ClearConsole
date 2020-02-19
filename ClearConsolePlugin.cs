96b5e20a85e1e5222895891fd439df0d57a3d5c9
Espcevan <espcevan@gmail.com@gmail.com>
2020-02-19 11:26:09 -0500

    First
    Upload of the Plugin

---
+++ ClearConsolePlugin.cs
@@ -0,0 +1,27 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using System;
+
+namespace Espcevan.ClearConsole
+{
+    public class ClearConsole : RocketPlugin
+    {
+        public static ClearConsole Instance;
+        public static string PluginName = "BuilderMode";
+        public static string PluginVersion = " 1.0.0";
+        protected override void Load()
+        {
+            Instance = this;
+            Logger.Log("ClearConsole has been loaded!");
+            Logger.Log(PluginName + PluginVersion, ConsoleColor.Yellow);
+            Logger.Log("Made by Espcevan", ConsoleColor.Yellow);
+        }
+
+        protected override void Unload()
+        {
+            Instance = null;
+            Logger.Log("BuilderMode has been unloaded!");
+            Logger.Log("Visit Tortellio Discord for more! https://discord.gg/pzQwsew", ConsoleColor.Yellow);
+        }
+    }
+}
---
+++ Commands/ClearConsoleCommand.cs
@@ -0,0 +1,41 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+
+namespace Espcevan.ClearConsole.Commands
+{
+    public class ClearConsole : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Console;
+
+        public string Name => "clear";
+
+        public string Help => "This command will clear the console.";
+
+        public string Syntax => string.Empty;
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string>() { "console.clear" };
+
+        public void Execute( IRocketPlayer caller, string[] command )
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Your Console has been Cleaned.");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("############################################################################");
+            Console.WriteLine("## dP     dP            dP                                             dP ##");
+            Console.WriteLine("## 88     88            88                                             88 ##");
+            Console.WriteLine("## 88     88 88d888b. d8888P dP    dP 88d888b. 88d888b. .d8888b. .d888b88 ##");
+            Console.WriteLine("## 88     88 88'  `88   88   88    88 88'  `88 88'  `88 88ooood8 88'  `88 ##");
+            Console.WriteLine("## Y8.   .8P 88    88   88   88.  .88 88       88    88 88.  ... 88.  .88 ##");
+            Console.WriteLine("## `Y88888P' dP    dP   dP   `88888P' dP       dP    dP `88888P' `88888P8 ##");
+            Console.WriteLine("############################################################################");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.Read();
+        }
+    }
+}
